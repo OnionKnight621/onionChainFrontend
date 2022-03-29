24b9d8a9ebbbbe7d7a8920d64d07e299020f0c57
onionknight621 <onionknight621@gmail.com>
2022-03-29 14:48:40 +0200

    upd: wallet polling

--- src/components/walletInfo/walletInfo.tsx
+++ src/components/walletInfo/walletInfo.tsx
@@ -1,5 +1,7 @@
-import { mainApiUri } from "../../config";
-import useFetch from "../../hooks/useFetch";
+import { useEffect } from "react";
+
+import { mainApiUri, pollInterval } from "../../config";
+import useLazyFetch from "../../hooks/useLazyFetch";
 import ErrorMessage from "../errorMessage/errorMessage";
 import Loader from "../loader/loader";
 
@@ -9,11 +11,22 @@ interface IWalletInfo {
 }
 
 const WalletInfo = () => {
-  const {
-    data: walletInfo,
-    loading,
-    error,
-  } = useFetch<IWalletInfo>(`${mainApiUri}/api/wallet-info`);
+  const [getWalletInfo, { data: walletInfo, loading, error }] =
+    useLazyFetch<IWalletInfo>(`${mainApiUri}/api/wallet-info`, {
+      useCache: false,
+    });
+
+  useEffect(() => {
+    getWalletInfo();
+
+    const interval = setInterval(() => {
+      getWalletInfo();
+    }, pollInterval);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   if (error) {
     return <ErrorMessage error={error} />;
